328edfedf77a9f71d2daca2e1104cebec296140b
NayoungKim1212 <kkd04250@naver.com>
2024-02-21 13:38:16 +0900

    feat todoAdd

---
+++ src/todoAdd.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <h2>To-do List</h2>
+  <!-- <form @submit="onSubmit" class="d-flex"> form은 자동으로 reload -->
+    <form @submit.prevent="onSubmit" class="d-flex"></form>
+    <div class="flex-grow-1 mr-2">
+      <input class = "form-control" type="text" v-model="todo" placeholder="Type new to-do"> 
+    </div>
+    <div>
+      <button class="btn btn-primary" type="submit">Add</button> 
+    </div>
+  </form>
+  {{ todos }}
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const todo = ref('')
+    const todos= ref([])
+    
+    const onSubmit = (e) => {
+    //  e.preventDefault();
+      todos.value.push({
+        id: Date.now(),
+        subject: todo.value,
+      }
+      );
+    };
+
+
+    return {
+      todo,
+      todos,
+      onSubmit,
+    };
+  },
+};
+</script>
+
+<style>
+.name {
+  color: red;
+}
+</style>
+
+
