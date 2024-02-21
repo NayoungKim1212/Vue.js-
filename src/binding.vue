21f92ba07bfe9862cc9f2b69076c0228151ce42b
NayoungKim1212 <kkd04250@naver.com>
2024-02-21 13:37:36 +0900

    feat v-bind

---
+++ src/binding.vue
@@ -0,0 +1,38 @@
+<template>
+    <div :class="nameClass">
+      {{ name }}
+    </div>
+    <input :type="type" :value="name"> 
+    <!---속성은 다 bind 가능, v-bind:속성값 => :속성값으로 축약 가능-->
+    <button class="btn btn-primary" @click="updateName">Click</button> 
+    <!--v-onclick => @로 축약 가능-->
+  </template>
+  
+  <script>
+  import { ref } from "vue";
+  export default {
+    setup() {
+      const name = ref('Happy')
+      const type = ref('number')
+      const nameClass = ref('name') // name을 지워주면 style의 color 변경 X
+  
+      const updateName = () => {
+        name.value = 'Corder'
+        type.value = 'text'
+      };
+  
+      return {
+        name,
+        updateName,
+        type,
+        nameClass,
+      };
+    },
+  };
+  </script>
+  
+  <style>
+  .name {
+    color: red;
+  }
+  </style>
