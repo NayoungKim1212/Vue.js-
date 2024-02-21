283ccd04edd60deee5d87ceb2d9f07e4f11ef4e0
NayoungKim1212 <kkd04250@naver.com>
2024-02-21 13:37:50 +0900

    feat v-for

---
+++ src/v-for.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <h2>To-do List</h2>
+    <form @submit.prevent="onSubmit" class="d-flex">
+      <div class="flex-grow-1 mr-2">
+        <input class="form-control" type="text" v-model="todo" placeholder="Type new to-do">
+      </div>
+      <div>
+        <button class="btn btn-primary" type="submit">Add</button>
+      </div>
+    </form>
+    <!-- {{ todos }} -->
+    <div v-for="todo in todos" :key="todo.id" class="card mt-2"> <!--접근하고자하는 array-->
+        <!--todos 안에 todo를 넣어주겠다(아무렇게나 적어도 상관X)-->
+        <!--v-for 사용 시 바인딩 필수(:key="todo.id")-->
+        <div class="card-body p-2">
+          {{ todo.subject }}
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const todo = ref('')
+    const todos = ref([
+      { id: 1, subject: "노트북 사기"},
+      { id: 2, subject: "키보드 사기"},
+    ]);
+
+    console.log(todos);
+
+    const onSubmit = () => {
+      // e.preventDefault();
+      todos.value.push({
+        id: Date.now(),
+        subject: todo.value,
+      }
+      );
+    };
+
+
+    return {
+      todo,
+      todos,
+      onSubmit,
+    };
+  },
+};
+</script>
+
+<style>
+.name {
+  color: red;
+}
+</style>
